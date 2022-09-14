9f639b7ae081a8f06146c9f7d51358045b0e06f2
Dietmar <eis0687.dietmar@gmail.com>
2022-09-14 17:08:32 +0200

    added sessionStorage for lang

--- src/context/LangContext.js
+++ src/context/LangContext.js
@@ -1,7 +1,11 @@
-import { useState, createContext  } from "react";
+import { useState, createContext, useEffect  } from "react";
 export const LangContext = createContext(); 
 export const LangContextProvider = ({ children }) => {
-  const [lang, setLang] = useState('en');
+  const [lang, setLang] = useState(sessionStorage.getItem('d.e.web.dev') || 'en');
+
+  useEffect(() => {
+    sessionStorage.setItem('d.e.web.dev', lang);
+  }, [lang]);
 
   return (
     <LangContext.Provider value={{ 
